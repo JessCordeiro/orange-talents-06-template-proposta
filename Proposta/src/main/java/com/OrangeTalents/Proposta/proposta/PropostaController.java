7dc6e820887552b84f227ec82b73ae5d0d0af2db
Jess <jessica_yara@yahoo.com.br>
2021-07-13 15:38:45 -0300

    Não-pode-haver-mais-de-uma-proposta

--- Proposta/src/main/java/com/OrangeTalents/Proposta/proposta/PropostaController.java
+++ Proposta/src/main/java/com/OrangeTalents/Proposta/proposta/PropostaController.java
@@ -1,9 +1,12 @@
 package com.OrangeTalents.Proposta.proposta;
 
+
+
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,9 +25,15 @@ public class PropostaController {
 	@PostMapping
 	@Transactional
 	ResponseEntity<?> cadastrar(@Valid @RequestBody PropostaRequest request, UriComponentsBuilder uriComponentsBuilder){
-		Proposta proposta = request.toModel();
+		if(repository.existsByDocumento(request.getDocumento())) {
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Já existe documento cadastrado");
 		
+	}else {
+
+		Proposta proposta = request.toModel();
 		repository.save(proposta);
 		return ResponseEntity.created(uriComponentsBuilder.path("/propostas/{id}").buildAndExpand(proposta.getId()).toUri()).build();
+
 	}
+		}
 }
--- Proposta/src/main/java/com/OrangeTalents/Proposta/proposta/PropostaRepository.java
+++ Proposta/src/main/java/com/OrangeTalents/Proposta/proposta/PropostaRepository.java
@@ -1,8 +1,10 @@
 package com.OrangeTalents.Proposta.proposta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PropostaRepository extends JpaRepository<Proposta, Long>{
 
-	
+	boolean existsByDocumento(String documento);
 }
